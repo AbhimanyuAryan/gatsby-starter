5537df6a978431dd68c0c2a7c6ab287733100bbe
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2019-01-09 04:13:22 +0530

    Added React File

---
+++ src/components/Counter.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    counter: 0,
+  };
+
+  onIncrement = () => {
+    this.setState((state) => ({ counter: state.counter + 1 }));
+  }
+
+  onDecrement = () => {
+    alert("Hello");
+    this.setState((state) => ({ counter: state.counter - 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.counter}
+        <button onClick={this.onIncrement} type="button">
+          Increment
+        </button>
+        <button onClick={this.onDecrement} type="button">
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
